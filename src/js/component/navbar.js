5c2facfd50cd5a97e6ac3719b25affcc43eb299f
amb-costa <ocaranzacosta@gmail.com>
2023-10-13 14:06:00 -0300

    basic Navbar done + creating first views

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-expand navbar-light bg-light mb-3">
+      <div className="container-fluid">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">Amber's Modern Life</span>
+        </Link>
+        <div>
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <button
+                className="btn btn-secondary dropdown-toggle"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Reviews
+              </button>
+              <ul className="dropdown-menu dropdown-menu-end">
+                <li>
+                  <Link className="dropdown-item" to="/revMusic">
+                    Music
+                  </Link>
+                </li>
+                <li>
+                  <Link className="dropdown-item" to="/revMovies">
+                    Movies
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
