8cafe61809bf07f6a02c19bc71847005934710d4
amb-costa <ocaranzacosta@gmail.com>
2023-10-14 21:54:59 -0300

    carousel is fixed, should apply styles. will turn every item into a component in the future

--- src/js/component/carousel.jsx
+++ src/js/component/carousel.jsx
@@ -1,45 +1,74 @@
 import React from "react";
 import Vaporwave from "../../img/vaporwave.jpeg";
+import SoulGlo from "../../img/soulGlo.jpeg";
 import "../../styles/home.css";
 
 const Carousel = () => {
   return (
-    <div
-      id="carouselExampleSlidesOnly"
-      className="carousel slide"
-      data-bs-ride="carousel"
-    >
+    <div id="carouselDesc" className="carousel slide">
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselDesc"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselDesc"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselDesc"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
       <div className="carousel-inner">
         <div className="carousel-item active">
-          <div className="bg-image">
-            <img
-              src={Vaporwave}
-              className="d-block"
-              style={{
-                opacity: "0.6",
-                height: "300px",
-                width: "100%",
-                objectFit: "cover",
-              }}
-              alt="vaporwave"
-            />
-            <div
-              className="mask"
-              style={{ backgroundColor: "hsla(0, 0%, 0%, 0.6)" }}
-            >
-              <div className="d-flex justify-content-center">
-                <span className="">text!</span>
-              </div>
-            </div>
+          <img src={Vaporwave} className="d-block w-100" alt="Vaporwave" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Latest</h5>
+            <p>Amber is drowning in the V A P O R W A V E</p>
           </div>
         </div>
         <div className="carousel-item">
-          <img src="..." className="d-block w-100" alt="..." />
+          <img src={SoulGlo} className="d-block w-100" alt="SoulGlo" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Music reviews</h5>
+            <p>Space here for the Soul Glo review</p>
+          </div>
         </div>
         <div className="carousel-item">
-          <img src="..." className="d-block w-100" alt="..." />
+          <img src="..." className="d-block w-100" alt="EdWood" />
+          <div className="carousel-caption d-none d-md-block">
+            <h5>Movie reviews</h5>
+            <p>Space here for the Ed Wood review</p>
+          </div>
         </div>
       </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselDesc"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselDesc"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
   );
 };
