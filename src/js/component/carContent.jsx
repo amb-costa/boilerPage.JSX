94921fb164d0053866b34e7c78531ab9f004a8fb
amb-costa <ocaranzacosta@gmail.com>
2023-10-24 00:37:48 -0300

    editing dimensions for images on carousel

--- src/js/component/carContent.jsx
+++ src/js/component/carContent.jsx
@@ -10,9 +10,15 @@ const CarContent = ({ img, alt, title, desc }) => {
   return (
     <div
       className={title == `Latest` ? "carousel-item active" : "carousel-item"}
+      style={{ height: "400px" }}
     >
-      <img src={img} className="d-block w-100" alt={alt} />
-      <div className="carousel-caption d-none d-md-block">
+      <img
+        src={img}
+        className="d-block w-100 h-100"
+        style={{ objectFit: "cover", objectPosition: "50% 25%" }}
+        alt={alt}
+      />
+      <div className="carousel-caption d-none d-md-block bg-secondary bg-opacity-50">
         <h5>{title}</h5>
         <p>{desc}</p>
       </div>
